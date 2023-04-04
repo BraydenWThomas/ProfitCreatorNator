6bf5d753529b6a841d4cffa39f4d216821bdfdc4
ali.bazzi <ali.bazzi@fdmgroup/com>
2023-04-04 14:03:12 +1000

    Initial commit

---
+++ profitcreatornator/src/main/java/com/fdmgroup/profitcreatornator/ProfitcreatornatorApplication.java
@@ -0,0 +1,13 @@
+package com.fdmgroup.profitcreatornator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfitcreatornatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfitcreatornatorApplication.class, args);
+	}
+
+}
---
+++ profitcreatornator/src/test/java/com/fdmgroup/profitcreatornator/ProfitcreatornatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fdmgroup.profitcreatornator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfitcreatornatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
