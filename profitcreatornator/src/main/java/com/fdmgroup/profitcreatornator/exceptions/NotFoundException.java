154c56b2290c271ed808643b23e3a12cf265b778
yingruiz-github <yingruiz@student.unimelb.edu.au>
2023-04-11 16:28:23 +1000

    added basic APIs

---
+++ profitcreatornator/src/main/java/com/fdmgroup/profitcreatornator/exceptions/NotFoundException.java
@@ -0,0 +1,9 @@
+package com.fdmgroup.profitcreatornator.exceptions;
+
+public class NotFoundException extends RuntimeException{
+
+	public NotFoundException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+}
