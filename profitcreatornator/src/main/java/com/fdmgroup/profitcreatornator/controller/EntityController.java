82ed96e2f02bb242a844676938ab45f2b600229b
ali.bazzi <ali.bazzi@fdmgroup/com>
2023-04-12 15:23:20 +1000

    Fixed brayden_1204

--- profitcreatornator/src/main/java/com/fdmgroup/profitcreatornator/controller/EntityController.java
+++ profitcreatornator/src/main/java/com/fdmgroup/profitcreatornator/controller/EntityController.java
@@ -10,7 +10,6 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -29,7 +28,6 @@ import com.fdmgroup.profitcreatornator.models.*;
 import com.fdmgroup.profitcreatornator.repository.*;
 
 @RestController
-@RequestMapping("/api")
 @CrossOrigin(origins = "*", maxAge = 3600)
 public class EntityController {
 	@Autowired
