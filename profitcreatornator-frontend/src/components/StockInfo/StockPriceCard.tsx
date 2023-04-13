51fba934703ac6e39db618a8ed68a643899b37b3
ali.bazzi <ali.bazzi@fdmgroup/com>
2023-04-13 11:59:02 +1000

    Stock Price component completed

---
+++ profitcreatornator-frontend/src/components/StockInfo/StockPriceCard.tsx
@@ -0,0 +1,35 @@
+import { BadgeDelta, Card, DeltaBar, DeltaType, Flex, Metric, Text } from "@tremor/react";
+import React from "react";
+import { FunctionComponent } from "react";
+
+interface Props {
+    symbol: string
+    deltaType: DeltaType
+    isIncreasePositive: boolean
+    stockPrice: string
+}
+
+
+const StockPriceCard: FunctionComponent<Props>= ({
+    symbol,
+    deltaType,
+    isIncreasePositive,
+    stockPrice
+}) => {
+    return(
+       <Card className="max-w-xs mb-1">
+        <Flex justifyContent="between" alignItems="center">
+            <Text>{symbol}</Text>
+            <BadgeDelta
+            deltaType={deltaType}
+            isIncreasePositive={isIncreasePositive}
+            size="sm">
+                +9.42
+            </BadgeDelta>
+        </Flex>
+        <Metric>{stockPrice}</Metric>
+       </Card>
+    );
+} 
+
+export default StockPriceCard;
