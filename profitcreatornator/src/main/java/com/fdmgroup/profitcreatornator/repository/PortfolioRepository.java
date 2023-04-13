52825faeddc784958b900f179224784de9d8b3bd
brayden.thomas <brayden.thomas@fdmgroup.com>
2023-04-13 16:42:52 +1000

    added some api calls for options and portfolio

--- profitcreatornator/src/main/java/com/fdmgroup/profitcreatornator/repository/PortfolioRepository.java
+++ profitcreatornator/src/main/java/com/fdmgroup/profitcreatornator/repository/PortfolioRepository.java
@@ -1,9 +1,18 @@
 package com.fdmgroup.profitcreatornator.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 
 import com.fdmgroup.profitcreatornator.models.Portfolio;
 
 public interface PortfolioRepository extends JpaRepository<Portfolio, Long>{
 
+	List<Portfolio> getByTraderId(@Param("traderId") long traderId);
+	
+	
+	
 }
