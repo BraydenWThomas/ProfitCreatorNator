3389db54cd7cdf6cf43e3907fd0d465a0dca183c
Robert Esquillon <robert.esquillon@fdmgroup.com>
2023-04-19 14:56:30 +1000

    More bells and whistles on portfolio.tsx

--- profitcreatornator-frontend/src/pages/dashboard.tsx
+++ profitcreatornator-frontend/src/pages/dashboard.tsx
@@ -2,7 +2,7 @@ import { Divider, Grid, Typography } from "@mui/material";
 
 import Navbar from '../components/Navbar';
 import DashStockCards from '../components/Dashboard/dashStockCards'
-import RecentTransactions from "@/components/Dashboard/RecentTransactionsTable";
+import RecentTransactions2 from "@/components/Dashboard/RecentTransactionsTable2";
 import Value from "@/components/Dashboard/value";
 import Gain from "@/components/Dashboard/gain";
 import Loss from "@/components/Dashboard/loss";
@@ -10,6 +10,9 @@ import ExchangeRates from "@/components/Dashboard/exchange";
 import Header from "@/components/Header";
 
 export default function dashboard() {
+
+    const noForUser = 1; // for sake of testing user without login
+
     return (
         <div className="dark:bg-slate-500" style={{ display: 'flex', width: "100%" }}>
             <div>
@@ -22,8 +25,8 @@ export default function dashboard() {
                         <Grid item sm={6}>
                             <DashStockCards />
                         </Grid>
-                        <Grid item sm={6} >
-                            <RecentTransactions />
+                        <Grid item sm={6}>
+                            <RecentTransactions2 active={noForUser} />
                         </Grid>
                         <Grid item sm={6}>
                             <Value />
